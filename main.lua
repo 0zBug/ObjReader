775c21dfcf45b6854236353ccd441c16b31a65af
Bug <77031032+0zBug@users.noreply.github.com>
2023-06-27 16:23:06 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,57 +1,96 @@
 
-return function(File)
-	local File = readfile(File .. ".obj")
+local function SplitData(Data)
+	return table.unpack(string.split(string.gsub(string.gsub(Data, "%a+%s+", ""), "%s+", " "), " "))
+end
 
-	local function GetData(Object, Type)
-	    local Data = {}
-	    
-	    for x, y, z in Object:gmatch(Type .. "%s+([%d%p]+)%s+([%d%p]+)%s+([%d%p]+)") do
-	        table.insert(Data, {x = x, y = y, z = z})
-	    end
-	    
-	    return Data
+local function VertexData(Face)
+	local Index, Coordinate = unpack(string.split(Face, "/"))
+
+	return tonumber(Index), tonumber(Coordinate)
+end
+
+return function(File)
+	local Objects = {}
+	local Materials = {}
+	local Material
+	
+	local function AddObject(Name)
+		table.insert(Objects, {
+			Name = Name,
+			Verticies = {},
+			Triangles = {},
+			Coordinates = {}
+		})
 	end
 	
-	local Verticies = {}
+	local Match = {
+		["mtllib%s+(.+)\.mtl"] = function(File)
+			for Material, File in next, MtlReader(File .. ".mtl") do
+				Materials[Material] = PngReader.new(readfile(File))
+			end
+		end,
+		["usemtl%s+%w+"] = function(usemtl) 
+			Material = string.gsub(usemtl, "usemtl%s+", "")
+		end,
+		["v%s+[\-\.%d]+%s+[\-\.%d]+%s+[\-\.%d]+"] = function(Vertex) 
+			table.insert(Objects[#Objects].Verticies, {SplitData(Vertex)})
+		end,
+		["vt%s+[\-\./%d]+%s+[\-\./%d]+"] = function(Coordinates) 
+			table.insert(Objects[#Objects].Coordinates, {SplitData(Coordinates)})
+		end,
+		["f%s+[\-/%d]+%s+[\-/%d]+%s+[\-/%d]+"] = function(Face)
+			local a, b, c = SplitData(Face)
+			
+			local a, at = VertexData(a)
+			local b, bt = VertexData(b)
+			local c, ct = VertexData(c)
+			
+			table.insert(Objects[#Objects].Triangles, {a, b, c, at, bt, ct, Materials[Material]})
+		end,
+		["object%s+(.+)"] = AddObject,
+		["o%s+(.+)"] = AddObject,
+		["g%s+(.+)"] = AddObject
+	}
 	
-	local Edges = {}
-	local Triangles = {}
-	
-	for _, Vertex in next, GetData(File, "v") do
-	    table.insert(Verticies, {Vertex.x, Vertex.y, Vertex.z})
-	end
-	   
-	for _, Face in next, GetData(File, "f") do
-		a, b, c = tonumber(Face.x:match("^%d+")), tonumber(Face.y:match("^%d+")), tonumber(Face.z:match("^%d+"))
-		table.insert(Triangles, {a, b, c})
+	for _, Line in next, string.split(readfile(File .. ".obj"), "\n") do
+		for Match, Callback in next, Match do
+			local Matched = string.match(Line, Match)
+			if Matched then
+				Callback(Matched)
+			end
+		end
 	end
 	
 	local function Rotate(rx, ry)
-		local cx, sx = math.cos(rx),math.sin(rx)
-		local cy, sy = math.cos(ry),math.sin(ry)
+		local cx, sx = math.cos(rx), math.sin(rx)
+		local cy, sy = math.cos(ry), math.sin(ry)
 		
-		for _, v in next, Verticies do
-			local x,y,z = v[1], v[2], v[3]
-			v[1], v[2], v[3] = x * cx - z * sx, y * cy - x * sx * sy - z * cx * sy, x * sx * cy + y * sy + z * cx * cy
+		for _, Object in next, Objects do
+			for _, v in next, Object.Verticies do
+				local x, y, z = v[1], v[2], v[3]
+				v[1], v[2], v[3] = x * cx - z * sx, y * cy - x * sx * sy - z * cx * sy, x * sx * cy + y * sy + z * cx * cy
+			end
 		end
 	end
 	
 	local function Scale(x, y, z)
-		for _, v in next, Verticies do
-			v[1], v[2], v[3] = v[1] * x, v[2] * y, v[3] * z
+		for _, Object in next, Objects do
+			for _, v in next, Object.Verticies do
+				v[1], v[2], v[3] = v[1] * x, v[2] * y, v[3] * z
+			end
 		end
 	end
 	
 	local function Translate(x, y, z)
-		for _, v in next, Verticies do
-			v[1], v[2], v[3] = v[1] + x, v[2] + y, v[3] + z
+		for _, Object in next, Objects do
+			for _, v in next, Object.Verticies do
+				v[1], v[2], v[3] = v[1] + x, v[2] + y, v[3] + z
+			end
 		end
 	end
 	
 	return {
-		Verticies = Verticies,
-		Edges = Edges,
-		Triangles = Triangles,
+		Objects = Object,
 		Rotate = Rotate,
 		Scale = Scale,
 		Translate = Translate
